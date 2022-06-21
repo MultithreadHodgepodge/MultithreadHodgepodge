f4b3069df16a7fd5b22a6aad791f24a3b4bbf6c3
Peter-Kung <410621208@gms.ndhu.edu.tw>
2022-06-21 19:39:05 +0800

    Build header file of simplethreadpool

--- simpleThreadpool/main.c
+++ simpleThreadpool/main.c
@@ -1,48 +1,5 @@
-#include "../list/list.h"
-#include <signal.h>
-#include <string.h>
-#include <semaphore.h>
-#include <pthread.h>
+#include "main.h"
 
-
-static const int threadQ = 4;
-static const int t = 5;
-static const int rq_capacity = 20;
-
-typedef void (*targetfun)();
-typedef struct Readyqueue {	
-	
-	int front, end;
-	sem_t item, remain;
-	pthread_mutex_t mutex;
-	targetfun *ringbuffer;
-
-}RQ_t;
-
-
-typedef struct thread_info {
-	pthread_t thread_id;
-	int thread_num;
-	char *argv_string;	
-	RQ_t *rq;
-
-}TINFO_t;
-
-void threadpool_init(TINFO_t **, RQ_t **);
-void readyqueue_init(RQ_t **);
-void foo1();
-void foo2();
-void foo3();
-void *worker(void *);
-void interrupt(int );
-void close_threadpool(TINFO_t **);
-void add_task(RQ_t **, int );
-void show(RQ_t **);
-targetfun task(RQ_t *);
-RQ_t *sigready_queue;
-targetfun select_job(int );
-
-int finish = 1;
 
 int main()
 {	
